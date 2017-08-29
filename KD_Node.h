aa92578437e0fb86a6b16ff4186b6c66d2bf826c
Alfred <alfredchavezurday@gmail.com>
2017-08-29 20:40:40 +0000

    Creating .h files of D. Structure

---
+++ KD_Node.h
@@ -0,0 +1,8 @@
+//
+// Created by alfred on 8/29/17.
+//
+
+#ifndef KD_TREE_KD_NODE_H
+#define KD_TREE_KD_NODE_H
+
+#endif //KD_TREE_KD_NODE_H
---
+++ KD_Tree.h
@@ -0,0 +1,8 @@
+//
+// Created by alfred on 8/29/17.
+//
+
+#ifndef KD_TREE_KD_TREE_H
+#define KD_TREE_KD_TREE_H
+
+#endif //KD_TREE_KD_TREE_H
