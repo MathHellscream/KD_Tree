d142c21435bc054daf65811e7e7fb334de2e8c87
Alfred <alfredchavezurday@gmail.com>
2017-09-03 17:34:24 +0000

    Structure and some functions defined

--- KD_Node.h
+++ KD_Node.h
@@ -10,11 +10,49 @@
 
 class KD_Node{
 private:
-    int dim;
-
-    KD_Node* Sons[2];
+    long dim;
+    std::string *Punto;
+    std::string *typeOfData;
 public:
-
+    KD_Node* Sons[2];
+    KD_Node(){
+        Punto = nullptr;
+        typeOfData = nullptr;
+        Sons[0] = nullptr;
+        Sons[1] = nullptr;
+        dim = 0;
+    }
+    KD_Node(std::vector<std::string> &data, std::string* &TypeOfData){
+        dim = (long)data.size();
+        Punto = new std::string[dim];
+        for (int i = 0; i < dim; ++i) {
+            Punto[i] = data[i];
+        }
+        this->typeOfData = TypeOfData;
+        Sons[0] = nullptr;
+        Sons[1] = nullptr;
+    }
+    void printData(){
+        for (int i = 0; i < dim; ++i) {
+            std::cout << Punto[i] << ' ';
+        }
+        std::cout << std::endl;
+        return;
+    }
+    std::vector<std::string> getData(){
+        std::vector<std::string> ans;
+        for (int i = 0; i < dim; ++i) {
+            ans.push_back(Punto[i]);
+        }
+        return ans;
+    }
+    std::string getData(long pos){
+        if(pos < 0 || pos > dim -1 || dim == 0) return "NoData!";
+        return Punto[pos];
+    }
+    ~KD_Node(){
+        delete[] Punto;
+    }
 };
 
 #endif //KD_TREE_KD_NODE_H
